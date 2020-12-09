9c2a978e5ae171b903e7c84f94d2e16575a65768
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-09 09:59:13 +0200

    Add home page js files and cities selector

---
+++ ParkUp/wwwroot/js/home.js
@@ -0,0 +1,16 @@
+﻿import { getCitiesArray } from './utilsAPI.js';
+
+refreshCitiesSelector();
+
+async function refreshCitiesSelector() {
+    let cities = await getCitiesArray();
+    $("#CitiesSelect").empty();
+    $("#CitiesSelect").append(`<option value="0">Select City...</option>`);
+    for (var i = 0; i < cities.length; i++) {
+        let element = `
+                        <option value="${cities[i].Id}">${cities[i].Name}</option>
+                      `;
+        $("#CitiesSelect").append(element);
+    }
+}
+
---
+++ ParkUp/wwwroot/js/utilsAPI.js
@@ -0,0 +1,17 @@
+﻿
+
+async function getCitiesArray() {
+    console.log("Getting cities...");
+    let cities = [];
+    let URL = `https://localhost:44315/api/cities`;
+    await $.getJSON(URL, function (data) {
+        for (var i = 0; i < data.length; i++) {
+            cities.push(data[i]);
+        }
+    })
+    console.log("Cities retrieved.");
+    return cities;
+}
+
+// export
+export {getCitiesArray}
