bfb064d74823f1b3a0ca3b6217eb7fcf24bcec09
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-13 10:21:11 +0200

    Remove Area Controller AddArea actions and views

--- ParkUp/Controllers/AreaController.cs
+++ ParkUp/Controllers/AreaController.cs
@@ -28,45 +28,5 @@ namespace ParkUp.Web.Controllers
             List<AreaViewModel> allAreasVM = _mapper.Map<List<Area>, List<AreaViewModel>>(areasFromDb);
             return View("AllAreas", allAreasVM);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> AddArea()
-        {
-            List<City> allCitiesFromDb = await _repository.GetAllCities();
-            List<CityViewModel> allCitiesVM = _mapper.Map<List<City>, List<CityViewModel>>(allCitiesFromDb);
-            ViewData["AllCitiesVM"] = allCitiesVM;
-            return View("AddArea");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddArea(AreaViewModel areaViewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    Area newArea = _mapper.Map<AreaViewModel, Area>(areaViewModel);
-                    await _repository.AddArea(newArea);
-                    CityArea newCityArea = new CityArea
-                    {
-                        Area = newArea,
-                        CityId = areaViewModel.CityId
-                    };
-                    await _repository.AddCityArea(newCityArea);
-                    return RedirectToAction("AllAreas");
-                }
-                catch (DbUpdateException dbex)
-                {
-                    ViewData["ErrorMessage"] = "DB issue - " + dbex.Message;
-                    return View("Error");
-                }
-                catch (Exception ex)
-                {
-                    ViewData["ErrorMessage"] = ex.Message;
-                    return View("Error");
-                }
-            }
-            return View("AddArea", areaViewModel);
-        }
     }
 }
