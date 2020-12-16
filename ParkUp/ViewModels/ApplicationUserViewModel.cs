ea9159404eeac9cc2be55a7a87955e342c3eb9f8
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-16 10:21:54 +0200

    Add DataAnnotations for ApplicationUser w/ Migration

--- ParkUp/ViewModels/ApplicationUserViewModel.cs
+++ ParkUp/ViewModels/ApplicationUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,13 +10,19 @@ namespace ParkUp.Web.ViewModels
     public class ApplicationUserViewModel
     {
         public string Id { get; set; }
+        [EmailAddress]
+        [MaxLength(150)]
         public string Email { get; set; }
         public DateTime DateAdded { get; set; }
         [Column(TypeName = "money")]
+        [Range(0, double.MaxValue, ErrorMessage = "Only positive values allowded.")]
         public decimal Credits { get; set; }
+        [MaxLength(100)]
         public string FirstName { get; set; }
+        [MaxLength(100)]
         public string LastName { get; set; }
         [Column(TypeName = "money")]
+        [Range(0, 100)]
         public decimal PartnerPercentage { get; set; }
     }
 }
