f8cee59804fa803b524da436a8ecac306d2c8092
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-16 10:27:21 +0200

    Add Area DataAnnotations w/ migration

---
+++ ParkUp.Infrastructure/Migrations/20201216082454_AreaDBAttributes.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkUp.Infrastructure.Migrations
+{
+    public partial class AreaDBAttributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Areas",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Areas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
