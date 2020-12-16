9910a1c015b780c540bd4e977287e73b87d1090d
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-16 13:41:46 +0200

    Add EditUser BE, FE validation

--- ParkUp/ViewModels/ApplicationUserViewModel.cs
+++ ParkUp/ViewModels/ApplicationUserViewModel.cs
@@ -10,6 +10,7 @@ namespace ParkUp.Web.ViewModels
     public class ApplicationUserViewModel
     {
         public string Id { get; set; }
+        [Required(ErrorMessage ="The Email Address is required.")]
         [EmailAddress]
         [MaxLength(150)]
         public string Email { get; set; }
@@ -17,12 +18,18 @@ namespace ParkUp.Web.ViewModels
         [Column(TypeName = "money")]
         [Range(0, double.MaxValue, ErrorMessage = "Only positive values allowed.")]
         public decimal Credits { get; set; }
+        [Required(ErrorMessage = "The First Name is required.")]
         [MaxLength(100)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "The Last Name is required.")]
         [MaxLength(100)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "The Partner Percentage is required.")]
+        [Display(Name = "Partner Percentage (ParkUp share)")]
         [Column(TypeName = "money")]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "The Partner Percentage must be between 0 and 100.")]
         public decimal PartnerPercentage { get; set; }
     }
 }
