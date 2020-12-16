45d9c930ba646affd3771c0b37ed38a40cc832a0
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-16 11:23:57 +0200

    Add Register BE/FE validation

--- ParkUp/ViewModels/RegisterViewModel.cs
+++ ParkUp/ViewModels/RegisterViewModel.cs
@@ -8,19 +8,23 @@ namespace ParkUp.Web.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
         [Display(Name = "First Name")]
+        [MaxLength(100)]
         public string FirstName { get; set; }
+        [Required]
         [Display(Name = "Last Name")]
+        [MaxLength(100)]
         public string LastName { get; set; }
         [Required]
         [EmailAddress]
+        [MaxLength(150)]
         [Display(Name = "Email (username)")]
         public string Email { get; set; }
-
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
+        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The Password and Password Confirmation don't match.")]
