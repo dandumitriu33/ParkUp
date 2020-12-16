a2aaab14dcc14523f658056f44b1e442474f24d0
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-16 13:05:15 +0200

    Add AddCity BE, FE validation

--- ParkUp/wwwroot/js/cities.js
+++ ParkUp/wwwroot/js/cities.js
@@ -24,27 +24,42 @@ $("#submitNewCity").click(addNewCity);
 
 async function addNewCity() {
     event.preventDefault();
-    console.log("Adding new city...");
-    let URL = `https://localhost:44315/api/cities`;
-    var newCityId = 0;
+    $("#addCityValidationErrorMessage").empty();
     var newCityName = $("#cityName").val();
-    var newCityAreas = [];
-    var obj = JSON.stringify({ Id: newCityId, Name: newCityName, Areas: newCityAreas });
-    await $.ajax({
-        type: "POST",
-        url: URL,
-        data: obj,
-        contentType: "application/json; charset=utf-8",
-        crossDomain: true,
-        success: function () {
-            console.log("City added successfully.");
-        },
-        error: function (jqXHR, status) {
-            console.log(jqXHR);
-            console.log('fail' + status.code);
-        }
-    });
-    console.log("City Added...");
-    console.log("Reactivating Pop cities...");
-    populateCities();
+    if (await validateNewCityName(newCityName) == false) {
+        $("#addCityValidationErrorMessage").text("The city name is required and must be less than 100 characters long.")
+    } else {
+        console.log("Adding new city...");
+        let URL = `https://localhost:44315/api/cities`;
+        var newCityId = 0;
+        var newCityAreas = [];
+        var obj = JSON.stringify({ Id: newCityId, Name: newCityName, Areas: newCityAreas });
+        await $.ajax({
+            type: "POST",
+            url: URL,
+            data: obj,
+            contentType: "application/json; charset=utf-8",
+            crossDomain: true,
+            success: function () {
+                console.log("City added successfully.");
+            },
+            error: function (jqXHR, status) {
+                console.log(jqXHR);
+                console.log('fail' + status.code);
+                $("#addCityValidationErrorMessage").text("Name length or connection error.")
+            }
+        });
+        console.log("City Added...");
+        console.log("Reactivating Pop cities...");
+        populateCities();
+    }
+}
+
+async function validateNewCityName(cityName) {
+    cityName.trim();
+    if (cityName.length > 0 && cityName.length < 100) {
+        return true;
+    } else {
+        return false;
+    }
 }
