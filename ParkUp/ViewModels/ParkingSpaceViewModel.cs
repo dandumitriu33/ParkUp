84a14d4c6d5a18e136d54fb257a5bd3d82fc97f8
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-16 10:49:20 +0200

    Add ParkingSpace DataAnnotations w/ migration

--- ParkUp/ViewModels/ParkingSpaceViewModel.cs
+++ ParkUp/ViewModels/ParkingSpaceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,14 +13,19 @@ namespace ParkUp.Web.ViewModels
         public int AreaId { get; set; }
         public bool IsApproved { get; set; }
         public string OwnerId { get; set; }
+        [MaxLength(100)]
         public string Name { get; set; }
+        [MaxLength(100)]
         public string StreetName { get; set; }
+        [MaxLength(100)]
         public string Description { get; set; }
         [Column(TypeName = "money")]
+        [Range(0, double.MaxValue, ErrorMessage = "Only positive values allowded.")]
         public decimal HourlyPrice { get; set; }
         public bool IsActive { get; set; } = false;
         public bool IsTaken { get; set; } = false;
         public DateTime DateAdded { get; set; }
+        [MaxLength(30)]
         public string GPS { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
