b2a6cae77d1eb32d27dbcf483ac9324169a1c6a1
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-17 15:11:54 +0200

    Add BuyCredits action exceptionn handling

--- ParkUp/Controllers/AccountController.cs
+++ ParkUp/Controllers/AccountController.cs
@@ -158,15 +158,15 @@ namespace ParkUp.Web.Controllers
                     await _repository.BuyCredits(userId, creditPack.Amount);
                     return View("BuyCredits");
                 }
-                catch (DbUpdateException dbex)
+                catch (DbUpdateException ex)
                 {
-                    ViewData["ErrorMessage"] = "DB issue - " + dbex.Message;
-                    return View("Error");
+                    ErrorViewModel newError = new ErrorViewModel() { ErrorMessage = ex.Message };
+                    return View("Error", newError);
                 }
                 catch (Exception ex)
                 {
-                    ViewData["ErrorMessage"] = ex.Message;
-                    return View("Error");
+                    ErrorViewModel newError = new ErrorViewModel() { ErrorMessage = ex.Message };
+                    return View("Error", newError);
                 }
             }
             return View("BuyCredits");
