8876b8b6e6b06f7df7fe547aea234ed6d16630c6
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 17:19:16 +0200

    Add CashOutComponent w/ link

--- ParkUp.Angular/parkup/src/app/components/owner/owner.module.ts
+++ ParkUp.Angular/parkup/src/app/components/owner/owner.module.ts
@@ -4,17 +4,20 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MyParkingSpacesComponent } from './my-parking-spaces.component';
 import { AddParkingSpaceComponent } from './add-parking-space.component';
+import { CashOutComponent } from './cash-out.component';
 
 const routes: Routes = [
   { path: 'my-parking-spaces', component: MyParkingSpacesComponent },
   { path: 'add-parking-space', component: AddParkingSpaceComponent },
+  { path: 'cash-out', component: CashOutComponent },
   
 ];
 
 @NgModule({
   declarations: [
     MyParkingSpacesComponent,
-    AddParkingSpaceComponent
+    AddParkingSpaceComponent,
+    CashOutComponent
   ],
   imports: [
     CommonModule,
