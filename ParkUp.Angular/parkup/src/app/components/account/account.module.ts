10cbee4db64323fc7cba0e2cda577994d05cb80b
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 12:13:22 +0200

    Add buy-credits component w/ link

--- ParkUp.Angular/parkup/src/app/components/account/account.module.ts
+++ ParkUp.Angular/parkup/src/app/components/account/account.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import { Routes,RouterModule } from '@angular/router';
 
 import { AccountComponent } from './account.component';
+import { BuyCreditsComponent } from './buy-credits.component';
 
 
 const routes: Routes = [
-  { path: 'account', component: AccountComponent }
+  { path: 'account', component: AccountComponent },
+  { path: 'buy-credits', component: BuyCreditsComponent }
 ];
 
 @NgModule({
-  declarations: [AccountComponent],
+  declarations: [
+    AccountComponent,
+    BuyCreditsComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
