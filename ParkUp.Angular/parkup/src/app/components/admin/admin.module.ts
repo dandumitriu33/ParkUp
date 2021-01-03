a93831476d630caedea594df190e5168352c8fd0
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 17:30:52 +0200

    Add AllSpacesComponent w/ link

--- ParkUp.Angular/parkup/src/app/components/admin/admin.module.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/admin.module.ts
@@ -5,18 +5,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { ApproveSpacesComponent } from './approve-spaces.component';
 import { ApproveCashOutComponent } from './approve-cash-out.component';
 import { AllUsersComponent } from './all-users.component';
+import { AllSpacesComponent } from './all-spaces.component';
 
 const routes: Routes = [
   { path: 'approve-spaces', component: ApproveSpacesComponent },
   { path: 'approve-cash-out', component: ApproveCashOutComponent },
   { path: 'all-users', component: AllUsersComponent },
+  { path: 'all-spaces', component: AllSpacesComponent },
 ];
 
 @NgModule({
   declarations: [
     ApproveSpacesComponent,
     ApproveCashOutComponent,
-    AllUsersComponent
+    AllUsersComponent,
+    AllSpacesComponent
   ],
   imports: [
     CommonModule,
---
+++ ParkUp.Angular/parkup/src/app/components/admin/all-spaces.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-spaces',
+  templateUrl: './all-spaces.component.html',
+  styleUrls: ['./all-spaces.component.css']
+})
+export class AllSpacesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
