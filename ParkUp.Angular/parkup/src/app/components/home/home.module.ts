a4eb30285e8dbd94daf9ff2447df4732ccaa3e48
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 18:29:32 +0200

    Add Home page Nearby and manual search sections html

--- ParkUp.Angular/parkup/src/app/components/home/home.module.ts
+++ ParkUp.Angular/parkup/src/app/components/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { SearchNearbyComponent } from './search-nearby.component';
+import { SelectAreaComponent } from './select-area.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SearchNearbyComponent
+    SearchNearbyComponent,
+    SelectAreaComponent
   ],
   imports: [
     CommonModule
