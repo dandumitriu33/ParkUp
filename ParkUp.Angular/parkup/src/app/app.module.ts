65bad499e925355f04afd617a333f9e6e553660c
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 11:49:14 +0200

    Add Account module

--- ParkUp.Angular/parkup/src/app/app.module.ts
+++ ParkUp.Angular/parkup/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './components/home/home.module';
+import { AccountModule } from './components/account/account.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeModule } from './components/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    AccountModule
   ],
   providers: [],
   bootstrap: [AppComponent]
