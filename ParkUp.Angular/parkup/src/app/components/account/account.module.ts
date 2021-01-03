2b2265a0c40feb7e37c81a051411c614d5690280
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 12:18:19 +0200

    Add Rental History component

--- ParkUp.Angular/parkup/src/app/components/account/account.module.ts
+++ ParkUp.Angular/parkup/src/app/components/account/account.module.ts
@@ -5,19 +5,22 @@ import { Routes,RouterModule } from '@angular/router';
 import { AccountComponent } from './account.component';
 import { BuyCreditsComponent } from './buy-credits.component';
 import { PurchaseHistoryComponent } from './purchase-history.component';
+import { RentalHistoryComponent } from './rental-history.component';
 
 
 const routes: Routes = [
   { path: 'account', component: AccountComponent },
   { path: 'buy-credits', component: BuyCreditsComponent },
-  { path: 'purchase-history', component: PurchaseHistoryComponent }
+  { path: 'purchase-history', component: PurchaseHistoryComponent },
+  { path: 'rental-history', component: RentalHistoryComponent }
 ];
 
 @NgModule({
   declarations: [
     AccountComponent,
     BuyCreditsComponent,
-    PurchaseHistoryComponent
+    PurchaseHistoryComponent,
+    RentalHistoryComponent
   ],
   imports: [
     CommonModule,
