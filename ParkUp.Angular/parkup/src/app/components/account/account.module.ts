6f21bfdbc58fbf6178ee02f8fa4f8eec287d5a92
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 12:26:25 +0200

    Add Privacy component

--- ParkUp.Angular/parkup/src/app/components/account/account.module.ts
+++ ParkUp.Angular/parkup/src/app/components/account/account.module.ts
@@ -6,13 +6,15 @@ import { AccountComponent } from './account.component';
 import { BuyCreditsComponent } from './buy-credits.component';
 import { PurchaseHistoryComponent } from './purchase-history.component';
 import { RentalHistoryComponent } from './rental-history.component';
+import { PrivacyComponent } from './privacy.component';
 
 
 const routes: Routes = [
   { path: 'account', component: AccountComponent },
   { path: 'buy-credits', component: BuyCreditsComponent },
   { path: 'purchase-history', component: PurchaseHistoryComponent },
-  { path: 'rental-history', component: RentalHistoryComponent }
+  { path: 'rental-history', component: RentalHistoryComponent },
+  { path: 'privacy', component: PrivacyComponent }
 ];
 
 @NgModule({
@@ -20,7 +22,8 @@ const routes: Routes = [
     AccountComponent,
     BuyCreditsComponent,
     PurchaseHistoryComponent,
-    RentalHistoryComponent
+    RentalHistoryComponent,
+    PrivacyComponent
   ],
   imports: [
     CommonModule,
