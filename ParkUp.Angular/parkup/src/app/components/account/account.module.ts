a7483132b3d00e184d7a08c9cad21a8bc051ba31
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 12:15:53 +0200

    Add Purchase History component w/ link

--- ParkUp.Angular/parkup/src/app/components/account/account.module.ts
+++ ParkUp.Angular/parkup/src/app/components/account/account.module.ts
@@ -4,17 +4,20 @@ import { Routes,RouterModule } from '@angular/router';
 
 import { AccountComponent } from './account.component';
 import { BuyCreditsComponent } from './buy-credits.component';
+import { PurchaseHistoryComponent } from './purchase-history.component';
 
 
 const routes: Routes = [
   { path: 'account', component: AccountComponent },
-  { path: 'buy-credits', component: BuyCreditsComponent }
+  { path: 'buy-credits', component: BuyCreditsComponent },
+  { path: 'purchase-history', component: PurchaseHistoryComponent }
 ];
 
 @NgModule({
   declarations: [
     AccountComponent,
-    BuyCreditsComponent
+    BuyCreditsComponent,
+    PurchaseHistoryComponent
   ],
   imports: [
     CommonModule,
