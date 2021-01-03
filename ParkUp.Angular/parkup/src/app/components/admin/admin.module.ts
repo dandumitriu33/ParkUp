f2e752a35b1d9dc76b25c37410fd7858450ab27e
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 17:29:46 +0200

    Add AllUsersComponent

--- ParkUp.Angular/parkup/src/app/components/admin/admin.module.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/admin.module.ts
@@ -4,16 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ApproveSpacesComponent } from './approve-spaces.component';
 import { ApproveCashOutComponent } from './approve-cash-out.component';
+import { AllUsersComponent } from './all-users.component';
 
 const routes: Routes = [
   { path: 'approve-spaces', component: ApproveSpacesComponent },
   { path: 'approve-cash-out', component: ApproveCashOutComponent },
+  { path: 'all-users', component: AllUsersComponent },
 ];
 
 @NgModule({
   declarations: [
     ApproveSpacesComponent,
-    ApproveCashOutComponent
+    ApproveCashOutComponent,
+    AllUsersComponent
   ],
   imports: [
     CommonModule,
