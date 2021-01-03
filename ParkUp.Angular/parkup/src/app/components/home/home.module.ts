7b39831d6bdd5b8348a8bff042e2b7d2b2684c1f
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-03 18:14:47 +0200

    Add Search Nearby component w/ html

--- ParkUp.Angular/parkup/src/app/components/home/home.module.ts
+++ ParkUp.Angular/parkup/src/app/components/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
+import { SearchNearbyComponent } from './search-nearby.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    SearchNearbyComponent
   ],
   imports: [
     CommonModule
---
+++ ParkUp.Angular/parkup/src/app/components/home/search-nearby.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-nearby',
+  templateUrl: './search-nearby.component.html',
+  styleUrls: ['./search-nearby.component.css']
+})
+export class SearchNearbyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
