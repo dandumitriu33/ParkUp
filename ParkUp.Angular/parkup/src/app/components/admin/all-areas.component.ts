53a300e088e6697852238802d954322e08c49c09
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-04 11:47:37 +0200

    Add AllAreas Angular complete feature

--- ParkUp.Angular/parkup/src/app/components/admin/all-areas.component.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/all-areas.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Area } from '../../models/Area';
+import { AreasService } from '../../services/areas.service';
+
 @Component({
   selector: 'app-all-areas',
   templateUrl: './all-areas.component.html',
   styleUrls: ['./all-areas.component.css']
 })
 export class AllAreasComponent implements OnInit {
+  allAreasForCity: Area[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  _cityIdSelect: string = '3';
+  get cityIdSelect(): string {
+    return this._cityIdSelect;
+  }
+  set cityIdSelect(value: string) {
+    this._cityIdSelect = value;
   }
 
+  constructor(private areasService:AreasService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  refreshAreasForCity(cityId: string) {
+    this.areasService.getAllAreasForCity(cityId).subscribe({
+      next: areas => {
+        this.allAreasForCity = areas;
+      },
+      error: err => console.error(err)
+    });
+  }
 }
---
+++ ParkUp.Angular/parkup/src/app/services/areas.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { Area } from '../models/Area';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AreasService {
+  private areasForCityUrl = `https://localhost:44315/api/areas/`;
+  //private cityId = '3';
+
+  constructor(private http: HttpClient) { }
+
+  getAllAreasForCity(cityId: string): Observable<Area[]> {
+    return this.http.get<Area[]>(this.areasForCityUrl + cityId).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
+
+
+}
