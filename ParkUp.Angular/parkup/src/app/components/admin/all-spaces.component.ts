ca2e07a6f1ae24b6c8e1ee880156eee98b67f005
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-04 14:26:19 +0200

    Add ParkingSpaces Component and Service logic

--- ParkUp.Angular/parkup/src/app/components/admin/all-spaces.component.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/all-spaces.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Area } from '../../models/Area';
+import { ParkingSpace } from '../../models/ParkingSpace';
+import { AreasService } from '../../services/areas.service';
+import { ParkingSpacesService } from '../../services/parking-spaces.service';
+
 @Component({
   selector: 'app-all-spaces',
   templateUrl: './all-spaces.component.html',
   styleUrls: ['./all-spaces.component.css']
 })
 export class AllSpacesComponent implements OnInit {
+  allAreasForCity: Area[];
+  allParkingSpacesForArea: ParkingSpace[];
 
-  constructor() { }
+  constructor(private parkingSpacesService: ParkingSpacesService,
+    private areasService: AreasService) { }
 
   ngOnInit(): void {
+  }
+
+  refreshAreasForCity(cityId: string) {
+    this.areasService.getAllAreasForCity(cityId).subscribe({
+      next: areas => {
+        this.allAreasForCity = areas;
+      },
+      error: err => console.error(err)
+    });
+  }
+
+  refreshParkingSpacesForArea(areaId: string) {
+    this.parkingSpacesService.getAllParkingSpacesForArea(areaId).subscribe({
+      next: parkingSpaces => {
+        this.allParkingSpacesForArea = parkingSpaces;
+      },
+      error: err => console.error(err)
+    });
   }
 
 }
--- ParkUp.Angular/parkup/src/app/services/parking-spaces.service.ts
+++ ParkUp.Angular/parkup/src/app/services/parking-spaces.service.ts
@@ -10,7 +10,25 @@ import { ParkingSpace } from '../models/ParkingSpace';
   providedIn: 'root'
 })
 export class ParkingSpacesService {
-  private parkingSpacesForAreaUrl = 'https://localhost:44315/api/parkingspaces/1/search/hoTel';
+  private parkingSpacesForAreaUrl = 'https://localhost:44315/api/parkingspaces/';
 
   constructor(private http: HttpClient) { }
+
+  getAllParkingSpacesForArea(areaId: string): Observable<ParkingSpace[]> {
+    return this.http.get<ParkingSpace[]>(this.parkingSpacesForAreaUrl + areaId + '/search/').pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
