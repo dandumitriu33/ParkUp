c5c850c380eefb090671a83729c20c9223990921
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-04 13:29:06 +0200

    Add ParkingSpace Angular model

---
+++ ParkUp.Angular/parkup/src/app/models/ParkingSpace.ts
@@ -0,0 +1,17 @@
+export class ParkingSpace {
+  Id: number;
+  AreaId: number;
+  IsApproved: boolean;
+  OwnerId: string;
+  Name: string;
+  StreetName: string;
+  Description: string;
+  HourlyPrice: number;
+  IsActive: boolean;
+  IsTaken: boolean;
+  DateAdded: string;
+  DateStarted: string;
+  GPS: string;
+  Latitude: number;
+  Longitude: number;
+}
