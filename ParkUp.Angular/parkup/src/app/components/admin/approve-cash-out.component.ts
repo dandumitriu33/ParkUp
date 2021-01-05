8ca4386c045206e72ebca1f4c5e6ce1af54b8811
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 14:07:42 +0200

    Add Angular ApproveCashOut component initial html

--- ParkUp.Angular/parkup/src/app/components/admin/approve-cash-out.component.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/approve-cash-out.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CashOut } from '../../models/CashOut';
+
 @Component({
   selector: 'app-approve-cash-out',
   templateUrl: './approve-cash-out.component.html',
   styleUrls: ['./approve-cash-out.component.css']
 })
 export class ApproveCashOutComponent implements OnInit {
+  allUnapprovedCashOuts: CashOut[];
 
   constructor() { }
 
