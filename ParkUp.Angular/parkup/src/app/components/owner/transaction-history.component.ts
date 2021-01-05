d49bafc7af01bf96320401f4b976265f151327fb
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 12:17:04 +0200

    Add Angular TransactionHistory component logic

--- ParkUp.Angular/parkup/src/app/components/owner/transaction-history.component.ts
+++ ParkUp.Angular/parkup/src/app/components/owner/transaction-history.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ParkingSpaceRental } from '../../models/ParkingSpaceRental';
+import { ParkingSpacesService } from '../../services/parking-spaces.service';
+
 @Component({
   selector: 'app-transaction-history',
   templateUrl: './transaction-history.component.html',
   styleUrls: ['./transaction-history.component.css']
 })
 export class TransactionHistoryComponent implements OnInit {
+  allTransactions: ParkingSpaceRental[];
 
-  constructor() { }
+  constructor(private parkingSpacesService: ParkingSpacesService) { }
 
   ngOnInit(): void {
+    this.parkingSpacesService.getAllOwnerTransactions().subscribe({
+      next: transactions => {
+        this.allTransactions = transactions;
+      },
+      error: err => console.error(err)
+    });
   }
 
 }
