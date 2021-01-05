1253791d89d1ddd0828a24a30ba03782b8d99fcb
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 13:39:44 +0200

    Add Angular ApproveSpaces component initial html

--- ParkUp.Angular/parkup/src/app/components/admin/approve-spaces.component.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/approve-spaces.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ParkingSpace } from '../../models/ParkingSpace';
+
 @Component({
   selector: 'app-approve-spaces',
   templateUrl: './approve-spaces.component.html',
   styleUrls: ['./approve-spaces.component.css']
 })
 export class ApproveSpacesComponent implements OnInit {
+  allUnapprovedParkingSpaces: ParkingSpace[];
 
   constructor() { }
 
