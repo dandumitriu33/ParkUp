95719e22ecceb94eb5815d916a3bd52997969365
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 10:57:46 +0200

    Add Angular Rental History component logic

--- ParkUp.Angular/parkup/src/app/components/account/rental-history.component.ts
+++ ParkUp.Angular/parkup/src/app/components/account/rental-history.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ParkingSpaceRental } from '../../models/ParkingSpaceRental';
+import { UsersService } from '../../services/users.service';
+
 @Component({
   selector: 'app-rental-history',
   templateUrl: './rental-history.component.html',
   styleUrls: ['./rental-history.component.css']
 })
 export class RentalHistoryComponent implements OnInit {
-  allRentals: any[];
+  allRentals: ParkingSpaceRental[];
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.usersService.getRentalHistory().subscribe({
+      next: rentals => {
+        this.allRentals = rentals;
+      },
+      error: err => console.error(err)
+    });
   }
 
 }
