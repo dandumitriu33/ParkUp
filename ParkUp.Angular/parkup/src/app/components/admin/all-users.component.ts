24f96638446669990c44d4becb8bcfc6a47c90ef
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 12:40:13 +0200

    Add Angular UsersService - getAllUsers API GET method

--- ParkUp.Angular/parkup/src/app/components/admin/all-users.component.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/all-users.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApplicationUser } from '../../models/ApplicationUser';
+
 @Component({
   selector: 'app-all-users',
   templateUrl: './all-users.component.html',
   styleUrls: ['./all-users.component.css']
 })
 export class AllUsersComponent implements OnInit {
-  allUsers: any[];
+  allUsers: ApplicationUser[];
 
   constructor() { }
 
--- ParkUp.Angular/parkup/src/app/services/users.service.ts
+++ ParkUp.Angular/parkup/src/app/services/users.service.ts
@@ -5,6 +5,7 @@ import { catchError, tap } from 'rxjs/operators';
 
 import { CreditPackPurchase } from '../models/CreditPackPurchase';
 import { ParkingSpaceRental } from '../models/ParkingSpaceRental';
+import { ApplicationUser } from '../models/ApplicationUser';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,18 @@ import { ParkingSpaceRental } from '../models/ParkingSpaceRental';
 export class UsersService {
   private purchaseHistoryUrl = 'https://localhost:44315/api/users/purchase-history/';
   private rentalHistoryUrl = 'https://localhost:44315/api/users/rental-history/';
+  private allUsersUrl = 'https://localhost:44315/api/users/users/all-users';
   // TEMPORARY hardcoded user ID
   private userId = '19a0694b-57eb-4b0a-aca4-86d71e389d0f';
 
   constructor(private http: HttpClient) { }
+
+  getAllUsers(): Observable<ApplicationUser[]> {
+    return this.http.get<ApplicationUser[]>(this.allUsersUrl).pipe(
+      tap(data => console.log('No of users: ' + data.length)),
+      catchError(this.handleError)
+    );
+  }
 
   getPurchaseHistory(): Observable<CreditPackPurchase[]> {
     return this.http.get<CreditPackPurchase[]>(this.purchaseHistoryUrl + this.userId).pipe(
