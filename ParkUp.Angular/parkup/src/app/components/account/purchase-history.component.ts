2ba12021fcdda9de25363aeb218fd6691418c493
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 10:14:13 +0200

    Add html and model for PurchaseHistory

--- ParkUp.Angular/parkup/src/app/components/account/purchase-history.component.ts
+++ ParkUp.Angular/parkup/src/app/components/account/purchase-history.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CreditPackPurchase } from '../../models/CreditPackPurchase';
+
 @Component({
   selector: 'app-purchase-history',
   templateUrl: './purchase-history.component.html',
   styleUrls: ['./purchase-history.component.css']
 })
 export class PurchaseHistoryComponent implements OnInit {
+  creditPackPurchaseHistory: CreditPackPurchase[];
 
   constructor() { }
 
