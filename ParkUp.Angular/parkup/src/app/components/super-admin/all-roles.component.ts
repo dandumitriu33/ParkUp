5acbf8aa02b1869e0a0ecce96bbd6a7ef6943a36
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 14:49:10 +0200

    Add Angular AllRoles component logic and html

--- ParkUp.Angular/parkup/src/app/components/super-admin/all-roles.component.ts
+++ ParkUp.Angular/parkup/src/app/components/super-admin/all-roles.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApplicationRole } from '../../models/ApplicationRole';
+import { UsersService } from '../../services/users.service';
+
 @Component({
   selector: 'app-all-roles',
   templateUrl: './all-roles.component.html',
   styleUrls: ['./all-roles.component.css']
 })
 export class AllRolesComponent implements OnInit {
+  allRoles: ApplicationRole[];
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.usersService.getAllRoles().subscribe({
+      next: roles => {
+        this.allRoles = roles;
+      },
+      error: err => console.error(err)
+    });
   }
 
 }
