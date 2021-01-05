9cc474cd00c63bd134fd5e60f20615db063ac930
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-05 13:45:05 +0200

    Add Angular ApproveSpaces component logic

--- ParkUp.Angular/parkup/src/app/components/admin/approve-spaces.component.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/approve-spaces.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ParkingSpace } from '../../models/ParkingSpace';
+import { ParkingSpacesService } from '../../services/parking-spaces.service';
 
 @Component({
   selector: 'app-approve-spaces',
@@ -10,9 +11,15 @@ import { ParkingSpace } from '../../models/ParkingSpace';
 export class ApproveSpacesComponent implements OnInit {
   allUnapprovedParkingSpaces: ParkingSpace[];
 
-  constructor() { }
+  constructor(private parkingSpacesService: ParkingSpacesService) { }
 
   ngOnInit(): void {
+    this.parkingSpacesService.getAllUnapprovedParkingSpaces().subscribe({
+      next: unapprovedParkingSpaces => {
+        this.allUnapprovedParkingSpaces = unapprovedParkingSpaces;
+      },
+      error: err => console.error(err)
+    });
   }
 
 }
