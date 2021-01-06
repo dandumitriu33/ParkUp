ca202722b4a7c0506044bda172923c23ed65ed5d
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-06 13:38:30 +0200

    Add Angular FormGroup acces via usersService injection - property

--- ParkUp.Angular/parkup/src/app/user/registration/registration.component.ts
+++ ParkUp.Angular/parkup/src/app/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-registration',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
   }
