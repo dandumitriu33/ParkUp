593c79a1024efccd3e5b6bbeea39a2723fc2e02b
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-09 18:40:41 +0200

    Add Angular AddParkingSpace - City and Area selector population

--- ParkUp.Angular/parkup/src/app/components/owner/add-parking-space.component.ts
+++ ParkUp.Angular/parkup/src/app/components/owner/add-parking-space.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Area } from '../../models/Area';
+
+import { City } from '../../models/City';
+import { AreasService } from '../../services/areas.service';
+import { CitiesService } from '../../services/cities.service';
 
 @Component({
   selector: 'app-add-parking-space',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-parking-space.component.css']
 })
 export class AddParkingSpaceComponent implements OnInit {
+  allCities: City[];
+  allAreas: Area[];
+  addParkingSpaceFormModel = {
+    CityId: '',
+    AreaId: '',
+    Name: '',
+    HourlyPrice: '',
+    StreetName: '',
+    Description: '',
+    GPS: ''
+  };
 
-  constructor() { }
+  constructor(private citiesService: CitiesService,
+              private areasService: AreasService) { }
 
   ngOnInit(): void {
+    this.citiesService.getAllCities().subscribe({
+      next: cities => {
+        this.allCities = cities;
+      },
+      error: err => console.error(err)
+    });
+  }
+
+  onCityChange(cityId: string) {
+    console.log('change city in add space: ' + cityId);
+    this.refreshAreasSelector(cityId);
+  }
+
+  refreshAreasSelector(cityId: string) {
+    console.log('refreshing areas for newly selected city: ' + cityId);
+    this.areasService.getAllAreasForCity(cityId).subscribe({
+      next: areas => {
+        this.allAreas = areas;
+      },
+      error: err => console.error(err)
+    });
   }
 
 }
