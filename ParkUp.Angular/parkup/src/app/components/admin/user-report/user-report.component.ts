f75e750499bf4d509052e7b10ff373fc510359d0
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-11 13:00:54 +0200

    Add Angular UserReportComponent w/ route

---
+++ ParkUp.Angular/parkup/src/app/components/admin/user-report/user-report.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-report',
+  templateUrl: './user-report.component.html',
+  styles: [
+  ]
+})
+export class UserReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
