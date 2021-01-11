43c8317e2eb777ebde87401babf15d6cbfce9428
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-11 14:38:46 +0200

    Add Angular UserReport daysAgoJoined calculation

--- ParkUp.Angular/parkup/src/app/components/admin/user-report/user-report.component.ts
+++ ParkUp.Angular/parkup/src/app/components/admin/user-report/user-report.component.ts
@@ -17,23 +17,39 @@ export class UserReportComponent implements OnInit {
     "FirstName": 'Loading...',
     "LastName": 'Loading...',
     "Email": 'Loading...',
-    "DateAdded": 'Loading...',
+    "DateAdded": '0',
     "Credits": 0.0,
     "PartnerPercentage": 0,
   };
+  daysAgoJoined: number;
 
   constructor(private usersService: UsersService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.paramMap.get('userId');
+    this.populateUserInfo();
+    
+  }
+
+  populateUserInfo() {
     this.usersService.getUserInfoForAdmin(this.userId).subscribe({
       next: userInfoFromDb => {
         console.log(`received: ${this.userInfo}`);
+        this.setDaysAgoJoined(userInfoFromDb.DateAdded);
         this.userInfo = userInfoFromDb;
       },
       error: err => console.error(err)
     });
+  }
+
+  setDaysAgoJoined(dateAdded: string) {
+    var date1 = new Date(dateAdded);
+    var date2 = new Date();
+
+    var Difference_In_Time = date2.getTime() - date1.getTime();
+    this.daysAgoJoined = Difference_In_Time / (1000 * 3600 * 24);
+    console.log(`>>>>>>>>diff in time ${this.daysAgoJoined}`);
   }
 
 }
