070f36f3f8de1fd58adb46334f4db58764ac1424
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-25 13:21:23 +0200

    Add Feture - User Logs on take/leave

---
+++ ParkUp.Core/CustomLog/CustomLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ParkUp.Core.CustomLog
+{
+    public class CustomLogger
+    {
+        public static void WriteToLogFile<T>(T logEntry, string filePath) where T : class
+        {
+            var cols = logEntry.GetType().GetProperties();
+            StringBuilder line = new StringBuilder();
+            foreach (var col in cols)
+            {
+                line.Append(col.GetValue(logEntry));
+                line.Append(" - ");
+            }
+            line.Append("\n");
+            if (File.Exists(filePath) ==  false)
+            {
+                File.WriteAllText(filePath, line.ToString());
+            }
+            File.AppendAllText(filePath, line.ToString());
+        }
+    }
+}
