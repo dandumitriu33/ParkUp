ae12581b92b2fa3908449238157d236367643dd8
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-25 12:49:27 +0200

    Add Core custom log folder and models

---
+++ ParkUp.Core/CustomLog/LogLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkUp.Core.CustomLog
+{
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Critical
+    }
+}
---
+++ ParkUp.Core/CustomLog/UserLogModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkUp.Core.CustomLog
+{
+    public class UserLogModel
+    {
+        public LogLevel Level { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeOfEvent { get; set; } = DateTime.UtcNow;
+    }
+}
